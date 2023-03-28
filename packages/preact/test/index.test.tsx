9527f4a2498eff0e558a0268ebf2b7ee320aa493
Andre Wiggins <andrewiggins@live.com>
2023-03-28 11:01:18 -0700

    Attempt to add repro of test for using a signal that only every written and reads are always on computed

--- packages/preact/test/index.test.tsx
+++ packages/preact/test/index.test.tsx
@@ -5,7 +5,8 @@ import {
 	useSignalEffect,
 	Signal,
 } from "@preact/signals";
-import { createElement, createRef, render } from "preact";
+import { createElement, createRef, FunctionComponent, render } from "preact";
+import { StateUpdater, useState } from "preact/hooks";
 import { setupRerender, act } from "preact/test-utils";
 
 const sleep = (ms?: number) => new Promise(r => setTimeout(r, ms));
@@ -188,6 +189,71 @@ describe("@preact/signals", () => {
 			sig.value = "bar";
 			rerender();
 			expect(spy).to.be.calledOnce;
+		});
+
+		it.only("should not subscribe to write-only signals", () => {
+			const location = signal<URL | null>(null);
+			const origin = computed(() => location.value?.origin);
+			const pathname = computed(() => location.value?.pathname);
+			const search = computed(() => location.value?.search);
+
+			const Origin = sinon.spy<FunctionComponent>(function Origin() {
+				// Manually read signal value so we can watch whether component rerenders
+				return <p>{origin.value}</p>;
+			});
+
+			const Pathname = sinon.spy<FunctionComponent>(function Pathname() {
+				// Manually read signal value so we can watch whether component rerenders
+				return <p>{pathname.value}</p>;
+			});
+
+			const Search = sinon.spy<FunctionComponent>(function Search() {
+				// Manually read signal value so we can watch whether component rerenders
+				return <p>{search.value}</p>;
+			});
+
+			let setLocation: StateUpdater<URL> = () => {};
+			const LocationProvider = sinon.spy<FunctionComponent>(
+				function SignalProvider({ children }) {
+					const [value, setValue] = useState(
+						new URL("https://domain.com/test?a=1")
+					);
+					setLocation = setValue;
+
+					location.value = value;
+					return children as any;
+				}
+			);
+
+			function App() {
+				return (
+					<LocationProvider>
+						<Origin />
+						<Pathname />
+						<Search />
+					</LocationProvider>
+				);
+			}
+
+			render(<App />, scratch);
+
+			expect(scratch.textContent).to.equal("https://domain.com/test?a=1");
+			expect(LocationProvider).to.be.calledOnce;
+			expect(Origin).to.be.calledOnce;
+			expect(Pathname).to.be.calledOnce;
+			expect(Search).to.be.calledOnce;
+
+			setLocation(u => {
+				u.search = "?a=2";
+				return new URL(u);
+			});
+			rerender();
+
+			expect(scratch.textContent).to.equal("https://domain.com/test?a=2");
+			expect(LocationProvider).to.be.calledTwice;
+			expect(Origin).to.be.calledOnce;
+			expect(Pathname).to.be.calledOnce;
+			expect(Search).to.be.calledTwice;
 		});
 	});
 
